388d9c95a9eae4ea2280bb23dba97883ff17f1c1
Florian Heuberger <fh@flogersoft.de>
2024-08-23 13:55:03 +0200

    added entity searching functions

--- src/main/java/com/fhtetris/fh_tetris/FhTetrisApplication.java
+++ src/main/java/com/fhtetris/fh_tetris/FhTetrisApplication.java
@@ -2,8 +2,14 @@ package com.fhtetris.fh_tetris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.fhtetris.fh_tetris.*")
+@ComponentScan(basePackages = { "com.fhtetris.fh_tetris.*" })
+@EntityScan(basePackages = "com.fhtetris.fh_tetris.*") // Scan complete package for entities
 public class FhTetrisApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fhtetris/fh_tetris/repositories/ScoreRepository.java
+++ src/main/java/com/fhtetris/fh_tetris/repositories/ScoreRepository.java
@@ -1,8 +1,10 @@
 package com.fhtetris.fh_tetris.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fhtetris.fh_tetris.entities.Score;
 
+@Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
 }
