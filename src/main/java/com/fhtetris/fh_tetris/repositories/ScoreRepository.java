055dfec30210aa887bc46637e96e69e46557240d
Florian Heuberger <fh@flogersoft.de>
2024-08-25 11:43:36 +0200

    added @Query for custom query

--- src/main/java/com/fhtetris/fh_tetris/repositories/ScoreRepository.java
+++ src/main/java/com/fhtetris/fh_tetris/repositories/ScoreRepository.java
@@ -1,10 +1,15 @@
 package com.fhtetris.fh_tetris.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fhtetris.fh_tetris.entities.Score;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Long> {
+    @Query(value = "SELECT s FROM Score s ORDER BY s.score DESC LIMIT 10")
+    List<Score> findAllByOrderByScoreDesc();
 }
