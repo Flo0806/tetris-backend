a6adae35da0271ea0dc478acebd4181befde687f
Florian Heuberger <fh@flogersoft.de>
2024-08-25 11:46:36 +0200

    changed cross origins

--- src/main/java/com/fhtetris/fh_tetris/controllers/ScoreController.java
+++ src/main/java/com/fhtetris/fh_tetris/controllers/ScoreController.java
@@ -11,26 +11,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/scores")
-@CrossOrigin(origins = "http://localhost:3000") // Erlaubt Cross-Origin-Anfragen vom Frontend
+@CrossOrigin(origins = { "http://localhost:3000", "https://fh-softdev.de", "https://www.fh-softdev.de",
+        "https://apps.fh-softdev.de" }) // Cross origin for the frontend
 public class ScoreController {
 
     @Autowired
     private ScoreRepository scoreRepository;
 
-    // Abrufen aller Scores
+    // Get all scores
     @GetMapping
     public List<Score> getAllScores() {
         return scoreRepository.findAll();
     }
 
-    // Speichern eines neuen Scores
+    // Save new score
     @PostMapping
     public ResponseEntity<Score> saveScore(@RequestBody Score score) {
         Score savedScore = scoreRepository.save(score);
         return ResponseEntity.ok(savedScore);
     }
 
-    // Abrufen eines spezifischen Scores
+    // Get top 10 scores ordered by score, desc
+    @GetMapping("/scores")
+    public List<Score> getAllScoresSortedByScoreDesc() {
+        return scoreRepository.findAllByOrderByScoreDesc();
+
+    }
+
+    // Get score by id
     @GetMapping("/{id}")
     public ResponseEntity<Score> getScoreById(@PathVariable Long id) {
         return scoreRepository.findById(id)
